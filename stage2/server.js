b3efa60bc5a385a765b40c7fdf84de1668a7fc9b
DanielVarjaskéri <varjaskerid@gmail.com>
2023-04-15 13:54:54 +0200

    router added

--- stage2/server.js
+++ stage2/server.js
@@ -5,13 +5,26 @@ const server = http.createServer((req,res)=>{
     console.log(req.method,req.url);
     //set header content type
      res.setHeader('Content-Type','text/html');
+     let path = './views/';
+     switch(req.url){
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        default:
+            path += '404.html';
+            break;        
+     }
     // res.write("<p>Hello My New Job</p>");
     // res.write("<h1>Hello My New Job</h1>");
     // res.write("<p>Hello My New Job</p>");
     // res.end();
-    fs.readFile('./views/index.html',(err,data)=>{
+    fs.readFile( `${path}`,(err,data)=>{
         if(err){
             console.log(err);
+            res.end();
         } else {
             res.write(data);
             res.end();
